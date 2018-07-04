078e28b39047162a7ae36a47b4c71cb98975746a
VincentTam97 <VincentTam97@users.noreply.github.com>
2018-07-04 18:33:16 +0800

    统计每一年的tech出现次数

--- GrabFieldKeywords.py
+++ GrabFieldKeywords.py
@@ -24,6 +24,18 @@ def generateCSVFiles():
                 writer.write(item[0] + ',' + str(item[1]) + '\n')
                 i = i + 1
 
+    return count_list
+
+
+
+
 
 if __name__ == '__main__':
-    generateCSVFiles()
+    fields = ['人工智能', '智能制造', '大数据', '云计算', '工业互联网', '网络安全', '集成电路', '物联网']
+    keyword_count = generateCSVFiles()
+
+    for i in range(8):
+        for top in range(8):
+            print(keyword_count[i][top][0])
+            tech_num = DU.countTechNumber(fields[1], keyword_count[i][top][0])
+            print(tech_num)
